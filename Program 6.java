3d9d11a9d3699e98afb9ea445e4c9b9afa09e4bb
sandeshagarwal <sandeshagarwal10@gmail.com>
2020-04-12 12:29:56 +0530

    P6 - Fibonacci Series (Recursive)
    Printing the Fibonacci Number at the position n ie. if n = 2 the result is 1 (0,1,1....)

---
+++ Program 6.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+static int fib(int n){
+if(n<=1)
+  return n;
+return fib(n-1)+fib(n-2);
+
+}
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the Number ");
+int n = sc.nextInt();
+int result = fib(n);
+System.out.println(result);
+}
+}
